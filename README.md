9004beeff906afebc2b6d895945367442dea87c6
John Camara <johnpcamara@new-host-3.home>
2012-08-26 20:25:54 -0400

    initial plugin generation using generate redmine_plugin task

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Redmine-Wiki-Repo-Traceability
 ==============================
 
 http://www.redmine.org/projects/redmine/wiki/Guide#Developer-guide
+
+ruby script/rails generate redmine_plugin Redmine_Wiki_Repo_Trace
---
+++ plugins/redmine_wiki_repo_trace/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_wiki_repo_trace do
+  name 'Redmine Wiki Repo Trace plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
