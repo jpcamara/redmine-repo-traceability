e07eeb6cdaa1c3ea0191a6738b2496a98d4a3e0b
jpcamara <johnpcamara@gmail.com>
2012-09-18 21:06:18 -0300

    Update plugins/redmine_wiki_repo_trace/init.rb

--- plugins/redmine_wiki_repo_trace/init.rb
+++ plugins/redmine_wiki_repo_trace/init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :redmine_wiki_repo_trace do
   author 'JP Camara, Kurt Barthel, Dave Mulcahey'
   description 'This plugin allows hooking together wikis and source code'
   version '0.0.1'
-  url 'https://github.com/jpcamara/Redmine-Wiki-Repo-Traceability'
+  url 'https://github.com/jpcamara/redmine-repo-traceability'
   author_url 'http://jpcamara.com'
 end
 
